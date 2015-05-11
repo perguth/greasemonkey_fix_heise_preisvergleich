207c5bd8dd075984a4ba3cc60ebf70099f1072fc
Per Guth <mail@perguth.de>
2015-05-11 18:32:31 +0200

    Working.

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
-var parseOrig = function (string) {
+function getSpecs () {
+  let name = 
+     document.getElementById('mitte_preisvergleicher')
+    .getElementsByTagName('h1')[0]
+    .querySelectorAll('.notrans')[0]
+    .firstChild.data
+  let specs = 
+     document.getElementById('gh_proddesc')
+    .querySelectorAll('.notrans')[0]
+    .firstChild.data
+  specs = parseSpecString(specs)
+  return {name, specs}
+}
+function parseSpecString (string) {
   string = string.split('•')
-  return specs.map(val => val.split(':'))
+  return string.map(val => val.split(':'))
+}
+function filterSpecs (val) {
+  if (
+  val[1].trim() === '' || 
+  val[1].trim() === 'N/A'
+  ) return false
+  return true
+}
+function fixSpec (val) {
+  switch (val.trim()) {
+    case 'zwei Jahre': return "24M"
+    case 'Windows 7 Professional 64bit': return "Win 7 Pro 64"
+    case 'Windows 8.1 Pro 64bit': return "Win 8.1 Pro 64"
+    case 'Windows 8.1 64bit': return "Win 8.1 64"
+    case 'USB 3.0': return "USB3"
+    case 'USB 2.0': return "USB2"
+    case 'Gb LAN': return "Gbit LAN"
+    case 'Bluetooth': return "BT"
+    case 'Megapixel': return "MP"
+    default: return val
+  }
+}
+function createNewSpecString() {
+  let {name, specs} = getSpecs()
+  specs = specs
+    .filter(filterSpecs)
+    .map(val => new Array(val[0], fixSpec(val[1])))
+    .map(val => val[0] + ': ' + val[1].trim())
+  return name + '<br><br>' + specs
+}
+function fixWebsite () {
+  document.getElementById('gh_proddesc')
+    .querySelectorAll('.notrans')[0].innerHTML = 
+    createNewSpecString()
 }
 
-var product = 
-   document.getElementById('mitte_preisvergleicher')
-  .getElementsByTagName('h1')[0]
-  .querySelectorAll('.notrans')[0]
-  .firstChild.data
-var specsOrig = 
-   document.getElementById('gh_proddesc')
-  .querySelectorAll('.notrans')[0]
-  .firstChild.data
-var specs = []
-var specsNew = ''
-
-//specsNew = product + "<br><br>" + specsOrig
-
-
-specs = specsOrig.split('•')
-for (var i = 0; i < specs.length; i++) {
-  specsNew += specs[i].split(':') + 'AAA'
-  //if (i % 2 === 0) specs[i] = specs[i].slice(0,-1)
-}
-
-
-// actually replace text:
-document.getElementById('gh_proddesc')
-  .querySelectorAll('.notrans')[0].innerHTML = specsNew
+fixWebsite()
 
