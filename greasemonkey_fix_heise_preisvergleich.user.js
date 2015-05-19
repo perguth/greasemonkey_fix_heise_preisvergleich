556a6d642fc9cd53ae21cb7bdb5e9dca8d27c7e3
Per Guth <mail@perguth.de>
2015-05-19 19:46:12 +0200

    ...

--- greasemonkey_fix_heise_preisvergleich.user.js
+++ greasemonkey_fix_heise_preisvergleich.user.js
@@ -7,6 +7,10 @@
 // @include     */fix_heise_preisvergleich/*
 // @version     1
 // @grant       none
+// @license     AGPLv3; https://www.gnu.org/licenses/why-affero-gpl.html
+// @homepageURL https://github.com/pguth/greasemonkey_fix_heise_preisvergleich
+// @supportURL  https://github.com/pguth/greasemonkey_fix_heise_preisvergleich/issues
+// @oujs:author pguth
 // ==/UserScript==
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
